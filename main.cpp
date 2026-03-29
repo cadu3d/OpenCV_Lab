a061667f194eaa954441ff7b2b31c98eee7c3d46
cadu3d <cadu3d@gmail.com>
2026-03-28 22:42:09 -0300

    Exercicio 8 - Versão Final

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <opencv2/core.hpp>
 #include <opencv2/opencv.hpp>
+#include <windows.h>
 
 void selectExerciceGUI();
 void runExercise(int exercise);
@@ -17,7 +18,7 @@ int main()
 {
     cv::utils::logging::setLogLevel(cv::utils::logging::LOG_LEVEL_ERROR);
 
-    std::cout << "Introducao a Computacao Grafica Lab 1 " "!\n";
+    std::cout << "Introducao a Computacao Grafica Lab 1" "!\n";
     std::cout << "OpenCV version: " << CV_VERSION << std::endl;
     selectExerciceGUI();
 }
@@ -61,7 +62,8 @@ void runExercise(int exercise)
         Exercise8();
         break;
     default:
-        std::cout << "Invalid exercise" << std::endl;
+        std::cout << "Exercicio Invalido" << std::endl;
+        selectExerciceGUI();
         break;
     }
 }
@@ -83,13 +85,12 @@ std::string buscarImagem()
     return pathImage;
 }
 
-cv::Mat generateGrayscale()
+cv::Mat gerarImagemCinza()
 {
     std::string pathImage = buscarImagem();
-    cv::Mat imageGray = cv::imread(pathImage, cv::IMREAD_GRAYSCALE);
-    cv::imwrite("../output/imagem_gray.jpg", imageGray);
-    std::cout << "A imagem em tons de cinza foi gravada em disco" << std::endl;
-    return imageGray;
+    cv::Mat imagemOriginal = cv::imread(pathImage, cv::IMREAD_GRAYSCALE);
+    cv::imwrite("../output/imagem_gray.jpg", imagemOriginal);
+    return imagemOriginal;
 }
 
 //Exercicios INICIO **********************************************************************
@@ -122,14 +123,14 @@ void Exercise2()
 
     //Converter para grayscale e gravar em disco
     //duvida: usar IMREAD_GRAYSCALE OU RGB2GRAY?
-    cv::Mat imageGray;
-    cv::cvtColor(imageRGB, imageGray, cv::COLOR_RGB2GRAY);
-    cv::imwrite("../output/imagem_gray.jpg", imageGray);
+    cv::Mat imagemOriginal;
+    cv::cvtColor(imageRGB, imagemOriginal, cv::COLOR_RGB2GRAY);
+    cv::imwrite("../output/imagem_gray.jpg", imagemOriginal);
     std::cout << "A imagem em tons de cinza foi gravada em disco" << std::endl;
 
     //Mostrar imagem colorida e convertida para tons de cinza
     mostrarImagem("Colorida", imageRGB, 2);
-    mostrarImagem("Tons de Cinza", imageGray, 2);
+    mostrarImagem("Tons de Cinza", imagemOriginal, 2);
 
     selectExerciceGUI();
 }
@@ -140,17 +141,17 @@ void Exercise3()
     std::cout << "Rodando o Exercicio 3" << std::endl;
 
     //ler e gravar a imagem em tons de cinza
-    cv::Mat imageGray = generateGrayscale();
+    cv::Mat imagemOriginal = gerarImagemCinza();
 
     // capturar coordenada
     int x, y;
-    std::cout << "Digite a coordenada y: ";
+    std::cout << "Digite a coordenada do pixel em y: ";
     std::cin >> y;
-    std::cout << "Digite a coordenada x: ";
+    std::cout << "Digite a coordenada do pixel em x: ";
     std::cin >> x;
 
     //capturar valor na coordenada e mostrar no console
-    int intensidade = imageGray.at<uchar>(y, x);
+    int intensidade = imagemOriginal.at<uchar>(y, x);
     std::cout << "intensidade na coordenada (" << y << "," << x << ") = " << intensidade << std::endl;
 
     selectExerciceGUI();
@@ -161,21 +162,21 @@ void Exercise4()
     std::cout << "Rodando o Exercicio 4" << std::endl;
 
     //ler e gravar a imagem em tons de cinza
-    cv::Mat imageGray = generateGrayscale();
+    cv::Mat imagemOriginal = gerarImagemCinza();
 
     //inverter a intensidade do pixel
     for (int y = 0; y < 512; ++y)
     {
         for (int x = 0; x < 512; ++x)
         {
-            uchar atual = imageGray.at<uchar>(y, x);
+            uchar atual = imagemOriginal.at<uchar>(y, x);
             uchar invertido = 255 - atual;
-            imageGray.at<uchar>(y, x) = invertido;
+            imagemOriginal.at<uchar>(y, x) = invertido;
         }
     }
 
     //mostrar imagem invertida
-    mostrarImagem("Invertida", imageGray, 2);
+    mostrarImagem("Invertida", imagemOriginal, 2);
 
     selectExerciceGUI();
 }
@@ -185,28 +186,28 @@ void Exercise5()
     std::cout << "Rodando o Exercicio 5" << std::endl;
 
     //ler e gravar a imagem em tons de cinza
-    cv::Mat imageGray = generateGrayscale();
+    cv::Mat imagemOriginal = gerarImagemCinza();
 
     //inverter a intensidade do pixel
     for (int y = 0; y < 512; ++y)
     {
         for (int x = 0; x < 512; ++x)
         {
-            uchar atual = imageGray.at<uchar>(y, x);
+            uchar atual = imagemOriginal.at<uchar>(y, x);
 
             if (atual > 128)
             {
-                imageGray.at<uchar>(y, x) = 255;
+                imagemOriginal.at<uchar>(y, x) = 255;
             }
             else
             {
-                imageGray.at<uchar>(y, x) = 0;
+                imagemOriginal.at<uchar>(y, x) = 0;
             }
         }
     }
 
     //mostrar imagem invertida
-    mostrarImagem("Preto & Branco", imageGray, 2);
+    mostrarImagem("Preto & Branco", imagemOriginal, 2);
 
     selectExerciceGUI();
 }
@@ -216,40 +217,35 @@ void Exercise6()
     std::cout << "Rodando o Exercicio 6" << std::endl;
 
     //Gerar imagem t.cinza e clonar
-    cv::Mat imageGray = generateGrayscale();
-    cv::Mat image128 = imageGray.clone();
-    cv::Mat image64 = imageGray.clone();
-    cv::Mat image16 = imageGray.clone();
-    cv::Mat image4 = imageGray.clone();
+    cv::Mat imagemOriginal = gerarImagemCinza();
+    cv::Mat image128 = imagemOriginal.clone();
+    cv::Mat image64 = imagemOriginal.clone();
+    cv::Mat image16 = imagemOriginal.clone();
+    cv::Mat image4 = imagemOriginal.clone();
 
     for (int y = 0; y < 512; ++y)
     {
         for (int x = 0; x < 512; ++x)
         {
-            uchar atual = imageGray.at<uchar>(y, x);
-            int total = 256;
+            uchar atual = imagemOriginal.at<uchar>(y, x);
             int divisor;
             int levels;
 
             levels = 128;
             divisor = 256 / levels;
-            uchar a128 = atual / 2;
             image128.at<uchar>(y, x) = (atual / divisor) * divisor;
 
             levels = 64;
             divisor = 256 / levels;
-            uchar a64 = (atual / divisor) * divisor;
-            image64.at<uchar>(y, x) = a64;
+            image64.at<uchar>(y, x) = (atual / divisor) * divisor;
 
             levels = 16;
             divisor = 256 / levels;
-            uchar a16 = (atual / divisor) * divisor;
-            image16.at<uchar>(y, x) = a16;
+            image16.at<uchar>(y, x) = (atual / divisor) * divisor;
 
             levels = 4;
             divisor = 256 / levels;
-            uchar a4 = (atual / divisor) * divisor;
-            image4.at<uchar>(y, x) = a4;
+            image4.at<uchar>(y, x) = (atual / divisor) * divisor;
         }
     }
 
@@ -264,50 +260,90 @@ void Exercise6()
     selectExerciceGUI();
 }
 
+int calcularMedia(cv::Mat imageTarget, int inicioY, int inicioX, int amostra)
+{
+    int total = 0;
+
+    for (int y = inicioY; y < inicioY + amostra; ++y)
+    {
+        for (int x = inicioX; x < inicioX + amostra; ++x)
+        {
+            total += imageTarget.at<uchar>(y, x);
+        }
+    }
+
+    return total / (amostra * amostra);
+}
+
+int preencheMedia(cv::Mat imageTarget, int inicioY, int inicioX, int amostra, int valor)
+{
+    int total = 0;
+
+    for (int y = inicioY; y < inicioY + amostra; ++y)
+    {
+        for (int x = inicioX; x < inicioX + amostra; ++x)
+        {
+            imageTarget.at<uchar>(y, x) = valor;
+        }
+    }
+
+    return total / (amostra * amostra);
+}
+
 void Exercise7()
 {
     std::cout << "Rodando o Exercicio 7" << std::endl;
 
     //ler e gravar a imagem em tons de cinza
-    cv::Mat imageGray = generateGrayscale();
+    cv::Mat imageOrig = gerarImagemCinza();
 
-    float pixelsAmostra;
+    //Obter tamanho da amostra
+    int amostra;
     float percentualRed;
-
-    std::cout << "percentual de reducao" << std::endl;
+    std::cout << "Digite o percentual de reducao (%)" << std::endl;
     std::cin >> percentualRed;
-    pixelsAmostra = 100 / percentualRed;
-    pixelsAmostra = std::round(pixelsAmostra);
+    amostra = 100 / (100 - percentualRed);
+    amostra = std::round(amostra);
 
-    int rows = imageGray.rows / pixelsAmostra;
-    int cols = imageGray.cols / pixelsAmostra;
-    cv::Mat imagemReduzida = cv::Mat(rows, cols, CV_8UC1);
+    //criar imagem reduzida
+    int imagemRedRows = imageOrig.rows / amostra;
+    int imagemRedCols = imageOrig.cols / amostra;
+    cv::Mat imagemRed = cv::Mat(imagemRedRows, imagemRedCols, CV_8UC1);
 
-    std::cout << "amostra pixel: " << pixelsAmostra << ", row col  " << rows << " " << cols << std::endl;
-    int vLength = pixelsAmostra * pixelsAmostra;
-    std::vector<uchar> v(vLength);
+    //criar imagem ampliada
+    int imagemAmpRows = imageOrig.rows;
+    int imagemAmpCols = imageOrig.cols;
+    cv::Mat imagemAmp = cv::Mat(imagemAmpRows, imagemAmpCols, CV_8UC1);
 
 
-    int index = 0;
-    for (int y = 0; y < pixelsAmostra; ++y)
+    //preencher a imagem reduzida com as medias de cada amostra
+    for (int y = 0; y < imagemRedRows; ++y)
     {
-        for (int x = 0; x < pixelsAmostra; ++x)
+        for (int x = 0; x < imagemRedCols; ++x)
         {
-            v[index++] = imageGray.at<uchar>(y, x);
+            int inicioY = y * amostra;
+            int inicioX = x * amostra;
+
+            //enviar amostra para calcular a media
+            int media = calcularMedia(imageOrig, inicioY, inicioX, amostra);
+            imagemRed.at<uchar>(y, x) = media;
+
+            //preenche imagem ampliada
+            preencheMedia(imagemAmp, inicioY, inicioX, amostra, media);
         }
     }
 
-    int total = 0;
-    for (int i = 0; i < vLength; ++i)
-    {
-        total = total + v[i];
-    }
-    int average = total / vLength;
+    /*Mostrar imagens
+    mostrarImagem("Imagem Original", imageOrig, 2);
+    mostrarImagem("Imagem Reduzida", imagemRed, 2);
+    mostrarImagem("Imagem Reduzida", imagemAmp, 2);
+    */
 
-    std::cout << "total: " << total << ", average: " << average << std::endl;
-
-    //mostrar imagem invertida
-    // mostrarImagem("Preto & Branco", imageGray, 2);
+    //Mostrar breve comparação
+    std::vector<cv::Mat> images = {imageOrig, imagemAmp};
+    cv::Mat comparcao;
+    cv::hconcat(images, comparcao); // all side by side
+    mostrarImagem("Preto & Branco", comparcao, 6);
 
     selectExerciceGUI();
 }
@@ -315,5 +351,77 @@ void Exercise7()
 void Exercise8()
 {
     std::cout << "Rodando o Exercicio 8" << std::endl;
+
+    //Gerar imagem t.cinza
+    cv::Mat imagemOriginal = gerarImagemCinza();
+
+    //Obter tamanho da amostra
+    int amostra;
+    float percentualRed = 30;
+    amostra = 100 / percentualRed;
+    amostra = std::round(amostra);
+
+    //criar imagem reduzida
+    int imagemRedRows = imagemOriginal.rows / amostra;
+    int imagemRedCols = imagemOriginal.cols / amostra;
+    cv::Mat imagemRed = cv::Mat(imagemRedRows, imagemRedCols, CV_8UC1);
+
+    //criar imagem ampliada
+    cv::Mat imagemAmp = cv::Mat(512, 512, CV_8UC1);
+
+
+    //preencher a imagem reduzida com as medias de cada amostra
+    for (int y = 0; y < imagemRedRows; ++y)
+    {
+        for (int x = 0; x < imagemRedCols; ++x)
+        {
+            int inicioY = y * amostra;
+            int inicioX = x * amostra;
+
+            //enviar amostra para calcular a media
+            int media = calcularMedia(imagemOriginal, inicioY, inicioX, amostra);
+            imagemRed.at<uchar>(y, x) = media;
+
+            //preenche imagem ampliada
+            preencheMedia(imagemAmp, inicioY, inicioX, amostra, media);
+        }
+    }
+
+
+    cv::Mat image20 = imagemOriginal.clone();
+    cv::Mat imagemFinal = imagemAmp.clone();
+
+
+    for (int y = 0; y < 512; ++y)
+    {
+        for (int x = 0; x < 512; ++x)
+        {
+            uchar atual = imagemOriginal.at<uchar>(y, x);
+            int divisor;
+            int levels;
+
+            levels = 20;
+            divisor = 256 / levels;
+            image20.at<uchar>(y, x) = (atual / divisor) * divisor;
+
+            uchar atual2 = imagemAmp.at<uchar>(y, x);
+            levels = 20;
+            divisor = 256 / levels;
+            imagemFinal.at<uchar>(y, x) = (atual2 / divisor) * divisor;
+
+        }
+    }
+
+    //Mostrar breve comparação
+    std::vector<cv::Mat> images = {imagemAmp, image20, imagemFinal};
+    cv::Mat comparcao;
+    cv::hconcat(images, comparcao); // all side by side
+    mostrarImagem("Preto & Branco", comparcao, 6);
+
+    std::cout << "A imagem de baixa resolução apresenta os contornos prejudicados (serrilhados), porém a qualidade dos preenchimentos permanece inalterada." << std::endl;
+    std::cout << "A imagem com poucos tons de cinza apresenta os contornos inalterados, porém a qualidade dos preenchimentos/superfícies demonstra efeitos de camadas." << std::endl;
+    std::cout << "A imagem de baixa resolução com poucos níveis de cinza apresenta os contornos prejudicados (serrilhados) e preenchimentos/superfícies com efeitos de camadas e serrilhamento entre as mesmas." << std::endl;
+
     selectExerciceGUI();
+
 }
